94369464378e61ffd6b56408f27fbdef8986a2de
tobi <tobi@error.(none)>
2011-05-23 13:37:35 +0200

    config npassen

--- config/db.php
+++ config/db.php
@@ -11,23 +11,14 @@ class etempus_db_config {
 		 * dsn-array
 		 * mehr unter: http://pear.php.net/manual/de/package.database.db.intro-dsn.php
 		 */
-		public $dsn = array(	
-								'phptype'  => "",
-								'mode'	   => false,
-								'dbsyntax' => false,
-								'username' => '',
-								'password' => '',
-								'protocol' => false,
-								'hostspec' => '',
-								'port'     => false,
-								'socket'   => false,
-								'database' => ''								
-							);
+// 		public $dsn = false;
 		
 		/*
 		 * optionen-array
 		 * mehr unter: http://pear.php.net/manual/de/package.database.db.db-common.setoption.php
 		 */
+		public $dsn = array(	'phptype'  => "sqlite");
+
 		public $options = array('debug'       => 2,
     							'portability' => DB_PORTABILITY_ALL,
 							   );
